fa9ecc4df1797bacb648c363277016cbbfe7a1c2
Connor <connordbrown@gmail.com>
2023-11-20 15:08:03 -0700

    complete lab

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,40 +1,100 @@
 class NationalPark:
 
+    all = []
+
     def __init__(self, name):
         self.name = name
+        NationalPark.all.append(self)
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if (not hasattr(self, 'name') and isinstance(name, str) and len(name) >= 3):
+            self._name = name
         
     def trips(self):
-        pass
+        return [trip for trip in Trip.all if trip.national_park == self]
     
     def visitors(self):
-        pass
+        return list(set(trip.visitor for trip in Trip.all if trip.national_park == self))
     
     def total_visits(self):
-        pass
+        if self.trips():
+            return len(self.trips())
+        else:
+            return 0
     
     def best_visitor(self):
-        pass
-
+        if self.visitors():
+            visits = {}
+            for tourist in self.visitors():
+                visits[tourist] = len([visit for visit in self.trips() if visit.visitor == tourist])
+            most_visits = max(visits.values())
+            top_visitors = [tourist for tourist, count in visits.items() if count == most_visits]
+            return top_visitors[0]
+        else:
+            return None
+    
+    @classmethod
+    def most_visited(cls):
+        visits = {}
+        for park in cls.all:
+            visits[park] = len(park.trips())
+        most_visits = max(visits.values())
+        top_visited = [park for park, count in visits.items() if count == most_visits]
+        return top_visited[0] if top_visited else None
 
 class Trip:
+
+    all = []
     
     def __init__(self, visitor, national_park, start_date, end_date):
         self.visitor = visitor
         self.national_park = national_park
         self.start_date = start_date
         self.end_date = end_date
+        Trip.all.append(self)
+    
+    @property
+    def start_date(self):
+        return self._start_date
+    
+    @start_date.setter
+    def start_date(self, start_date):
+        if isinstance(start_date, str) and len(start_date) >= 7:
+            self._start_date = start_date
 
+    @property
+    def end_date(self):
+        return self._end_date
+    
+    @end_date.setter
+    def end_date(self, end_date):
+        if isinstance(end_date, str) and len(end_date) >= 7:
+            self._end_date = end_date
 
 class Visitor:
 
     def __init__(self, name):
         self.name = name
         
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and (1 <= len(name) <= 15):
+            self._name = name
+
     def trips(self):
-        pass
+        return [trip for trip in Trip.all if trip.visitor == self]
     
     def national_parks(self):
-        pass
+        return list(set(trip.national_park for trip in Trip.all if trip.visitor == self))
     
     def total_visits_at_park(self, park):
-        pass
+        return len([trip for trip in park.trips() if trip.visitor == self])
--- lib/debug.py
+++ lib/debug.py
@@ -6,6 +6,11 @@ from classes.many_to_many import Visitor
 from classes.many_to_many import Trip
 
 if __name__ == '__main__':
-    print("HELLO! :) let's debug :vibing_potato:")
+    p_1 = NationalPark("Yosemite")
+    p_2 = NationalPark("Yellow Stone")
+    vis_1 = Visitor("Tom")
+    Trip(vis_1, p_1, "May 5th", "May 9th")
+    Trip(vis_1, p_1, "January 5th", "January 20th")
+    Trip(vis_1, p_2, "January 25th", "January 30th")
 
     ipdb.set_trace()
